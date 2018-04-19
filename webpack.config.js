2389942e3953b83f5935e0a1e12441920d3e40d7
Andre <25297095+xvzero@users.noreply.github.com>
2018-04-19 16:51:17 -0700

    remove public folder

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 const webpack = require('webpack');
 
 module.exports = {
-  entry: path.join(__dirname, './public/js/index.js'),
+  entry: path.join(__dirname, './js/index.js'),
   output: {
     filename: 'bundle.js',
     path: path.join(__dirname, './')
